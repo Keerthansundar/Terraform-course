689ffa980f522b9e4c59cf3450b5933cf7ed165b
Keerthan <2492157@cognizant.com>
2026-06-04 22:54:21 +0530

    doneee

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
 output "ec2_public_ip" {
   value = [
-    for i in aws_aws_instance.my_instance : i.public_ip
+    for i in aws_instance.my_instance : i.public_ip
   ]
 }
 
 output "ec2_public_dns" {
   value = [
-    for i in aws_aws_instance.my_instance : i.public_dns
+    for i in aws_instance.my_instance : i.public_dns
   ]
 }
 
 output "ec2_private_ip" {
   value = [
-    for i in aws_aws_instance.my_instance : i.private_ip
+    for i in aws_instance.my_instance : i.private_ip
   ]
 }
 
