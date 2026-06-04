a02c78da800c52007d582c2ca6b1978e9526384b
Keerthan <2492157@cognizant.com>
2026-06-04 22:52:08 +0530

    minor edit on varibales files

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,18 @@
 output "ec2_public_ip" {
-  value = aws_instance.my_instance[*].public_ip
+  value = [
+    for i in aws_aws_instance.my_instance : i.public_ip
+  ]
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.my_instance[*].public_dns
+  value = [
+    for i in aws_aws_instance.my_instance : i.public_dns
+  ]
 }
 
 output "ec2_private_ip" {
-  value = aws_instance.my_instance[*].private_ip
+  value = [
+    for i in aws_aws_instance.my_instance : i.private_ip
+  ]
 }
+
